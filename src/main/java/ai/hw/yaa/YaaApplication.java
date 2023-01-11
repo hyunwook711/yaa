da77b70c0422cdb6f109267c72da7b1acdd1ca08
Kimhyunwook <hyunwook711@iotcube.com>
2023-01-11 16:17:01 +0900

    first yaa

---
+++ src/main/java/ai/hw/yaa/YaaApplication.java
@@ -0,0 +1,14 @@
+package ai.hw.yaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("print");
+		SpringApplication.run(YaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/hw/yaa/YaaApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.hw.yaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
